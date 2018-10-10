5055bf0d3ffaaad241f7ceb5f2b732e179665b08
John Kim <kim.jy.john@gmail.com>
2018-10-09 17:27:38 -0700

    Prettier

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,31 +1,27 @@
-import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import uuid from "uuid";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
 
 const initialState = {
-  items: [
-    { id: uuid(), name: 'Eggs' },
-    { id: uuid(), name: 'Milk' },
-    { id: uuid(), name: 'Steak' },
-    { id: uuid(), name: 'Candy' }
-  ]
-}
+  items: [],
+  loading: false
+};
 
 export default function(state = initialState, action) {
-  switch(action.type) {
+  switch (action.type) {
     case GET_ITEMS:
       return {
         ...state
-      }
+      };
     case DELETE_ITEM:
       return {
         ...state,
         items: state.items.filter(item => item.id !== action.payload)
-      }
+      };
     case ADD_ITEM:
       return {
         ...state,
         items: [action.payload, ...state.items]
-      }
+      };
     default:
       return state;
   }
