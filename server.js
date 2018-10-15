245153a871f8812d68ba87d7f3e89cc32f0aff6c
John Kim <kim.jy.john@gmail.com>
2018-10-15 14:37:46 -0700

    Beautify

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const items = require('./routes/api/items')
+const items = require("./routes/api/items");
 
 const app = express();
 
@@ -10,16 +10,22 @@ const app = express();
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to mongo
 mongoose
   .connect(db)
-  .then(() => console.log('MongoDB Connected...'))
+  .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
 // Use routes
-app.use('/api/items', items)
+app.use("/api/items", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static('client/build'));
+}
 
 const port = process.env.PORT || 5000;
 
